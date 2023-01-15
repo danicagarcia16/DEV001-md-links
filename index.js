3da526ba8369b90cbd268866abbad03466a56e8d
danicagarcia16 <danicagarcia1613@gmail.com>
2023-01-15 11:33:43 -0500

    npm init

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const mdLinks = (path, options) =>{
+    return new Promise((resolve, reject) => {
+        //Ingresó el path o ruta
+        //verificar si existe la ruta o ptah
+        if (fs.existSync(path)) {
+           
+          } else {
+            reject(new Error("La ruta no existe"));
+          } 
+      
+    });
+
+
+ 
+}
+module.exports =  {
+mdLinks
 };
